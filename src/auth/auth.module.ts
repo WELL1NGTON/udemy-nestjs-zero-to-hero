2da0bb72bdb28c1179e3a8b0659425cfcfd8c56b
Wellington Carlos Massola <wellingtonmassola@outlook.com>
2021-10-08 08:52:44 -0300

    feat: finalizada a seção 5 do curso
    Seção 5 equivalente a branch s4-auth-part-1.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -12,7 +13,8 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({ secret: 'topSecret51', signOptions: { expiresIn: 3600 } }),
     TypeOrmModule.forFeature([UsersRepository]),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
